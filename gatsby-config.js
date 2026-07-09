a52898372b42917747d8dc13ddb563eb8f5b4feb
Jacob Kraniak <113630934+jacob-kraniak@users.noreply.github.com>
2026-07-09 09:29:16 -0400

    Add pathPrefix to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: "Professional portfolio showcasing cybersecurity expertise, projects, and experience.",
     siteUrl: "https://jacob-kraniak.github.io",
   },
+  pathPrefix: "/",  // Explicit for user site
   plugins: [
     "gatsby-plugin-react-helmet",
   ],
