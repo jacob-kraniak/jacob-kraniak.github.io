3e6b55de3b2b74803f3b0fa478c8d8b6e13989e6
Jacob Kraniak <113630934+jacob-kraniak@users.noreply.github.com>
2026-07-09 13:56:08 +0000

    minor change to js config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,9 @@
-module.exports = {
-  siteMetadata: {
-    title: "Jacob Kraniak | Cybersecurity Portfolio",
-    description: "Professional portfolio showcasing cybersecurity expertise, projects, and experience.",
-    siteUrl: "https://jacob-kraniak.github.io",
-  },
-  pathPrefix: "/",  // Explicit for user site
-  plugins: [
-    "gatsby-plugin-react-helmet",
-  ],
+export const siteMetadata = {
+  title: "Jacob Kraniak | Cybersecurity Portfolio",
+  description: "Professional portfolio showcasing cybersecurity expertise, projects, and experience.",
+  siteUrl: "https://jacob-kraniak.github.io",
 };
+export const pathPrefix = "/";
+export const plugins = [
+  "gatsby-plugin-react-helmet",
+];
